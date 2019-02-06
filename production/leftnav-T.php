7a6c2d89443c463c488c9718fd62f44eae33e511
root <premsai.sala@gmail.com>
2019-02-06 11:03:17 +0530

    updated Leftnav

--- production/leftnav-T.php
+++ production/leftnav-T.php
@@ -14,7 +14,7 @@
         <ul class="nav child_menu">
           <li><a href="tickets-T.php">Active Tickets</a></li>
           <li><a href="ticketsresolved-T.php">Resolved Tickets</a></li>
-          <li><a href="escilated-T.php">Escilated Tickets</a></li>
+          <li><a href="escalated-T.php">Escilated Tickets</a></li>
           <!-- <li><a href="">Form Wizard</a></li>
           <li><a href="">Form Upload</a></li>
           <li><a href="">Form Buttons</a></li> -->
